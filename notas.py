2253d61c0b674b629ba32637e5bfb292638d0cf4
Yagnih <gabrielbellochi@gmail.com>
2025-10-28 23:29:14 -0300

    meu fi quer oque?

--- notas.py
+++ notas.py
@@ -1,5 +1,10 @@
-escolha =int(input()) #abre uma caixa onde pode ser escrito um numero ou letra
-if escolha == 1 or escolha == 2 or escolha == 3:#aqui se a escolha for 1 2 ou 3 fará o comando abaixo
-    print("meu fi que leite?" )
-else:
+escolha =(input()) #abre uma caixa onde pode ser escrito um numero ou letra
+if escolha == "meu fi quer comer?":#aqui se a escolha for 1 2 ou 3 fará o comando abaixo
+    print("não quero naum" )
+elif escolha == "meu fi quer leite?": 
     print("num quero nada")
+elif escolha == "meu fi quer cachaça?":
+    print("me de papai")
+else: 
+    print ("pergunta oque seu fi quer")
+
