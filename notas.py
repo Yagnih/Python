bc349dd5b0f115f339078946255f983d9c659caf
Yagnih <gabrielbellochi@gmail.com>
2025-10-28 23:18:58 -0300

    meu primeiro programa

---
+++ notas.py
@@ -0,0 +1,5 @@
+escolha =int(input()) #abre uma caixa onde pode ser escrito um numero ou letra
+if escolha == 1 or escolha == 2 or escolha == 3:#aqui se a escolha for 1 2 ou 3 fará o comando abaixo
+    print("vai se fuder lula" )
+else:
+    print("bolsonaro meu presidente")
