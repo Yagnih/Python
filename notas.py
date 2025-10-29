9d0cdf515553ad39e60fed8ee0d0cd12f88bc037
Yagnih <gabrielbellochi@gmail.com>
2025-10-28 23:21:50 -0300

    meu fique oque?

--- notas.py
+++ notas.py
@@ -1,5 +1,5 @@
 escolha =int(input()) #abre uma caixa onde pode ser escrito um numero ou letra
 if escolha == 1 or escolha == 2 or escolha == 3:#aqui se a escolha for 1 2 ou 3 fará o comando abaixo
-    print("vai se fuder lula" )
+    print("meu fi que leite?" )
 else:
-    print("bolsonaro meu presidente")
+    print("num quero nada")
